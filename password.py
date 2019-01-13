e04dace7e7eb19c9e13036c1b12ff46b4e236e79
liuru8968 <liuru8968@gmail.com>
2019-01-14 01:32:12 +0800

    密碼重試程式

---
+++ password.py
@@ -0,0 +1,12 @@
+i = 3
+password='a123456'
+while True:
+    pwd=input ('請輸入密碼:')
+    if pwd == password:
+        print('密碼正確') 
+        break
+    else:
+    	i = i - 1
+    	print('密碼錯誤! 還剩',i,'次機會')
+    	if i == 0:
+    	   break
