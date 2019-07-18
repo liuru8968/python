bfd0bae18409fb92edacdcc30d2dba2f7b8ef416
劉茹馨 <46584638+liuru8968@users.noreply.github.com>
2019-07-18 23:19:14 +0800

    利用pH值指示劑做範例

--- Dictionary/Dictionary_1.py
+++ Dictionary/Dictionary_1.py
@@ -7,7 +7,7 @@
 ph_indicator = {'4':'紅色', '5':'橘色', '6':'黃色', '7':'綠色', 
 				'8':'藍色', '9':'靛色'}
 
-ph_indicator['10'] = '紫色'#add a key
+ph_indicator['10'] = '紫色'#增加一個key
 
 print(ph_indicator['5'])#顯示字典
 print(ph_indicator['10'])#如果不包含字典中，中途加進字典也會顯示出來
