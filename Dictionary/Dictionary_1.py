a33a3f8ef92630c173ed70506ff8306806de3c3c
劉茹馨 <46584638+liuru8968@users.noreply.github.com>
2019-07-18 23:17:44 +0800

    字典的應用範例

---
+++ Dictionary/Dictionary_1.py
@@ -0,0 +1,13 @@
+#Dictionary 字典 應用範例ex:1
+#利用pH指示劑做範例
+
+#新增一個字典使用{}
+#key和value的對應,key就是我們指定的名稱，value就是根據key找到的資料
+
+ph_indicator = {'4':'紅色', '5':'橘色', '6':'黃色', '7':'綠色', 
+				'8':'藍色', '9':'靛色'}
+
+ph_indicator['10'] = '紫色'#add a key
+
+print(ph_indicator['5'])#顯示字典
+print(ph_indicator['10'])#如果不包含字典中，中途加進字典也會顯示出來
